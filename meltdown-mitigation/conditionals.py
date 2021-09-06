5796dfee1c6fab76272db1842cbad3a9950c5595
Charles Roper <charles@roper.im>
2021-09-06 01:59:31 +0100

    Meltdown Mitigation

--- meltdown-mitigation/conditionals.py
+++ meltdown-mitigation/conditionals.py
@@ -1,53 +1,111 @@
-def is_criticality_balanced(temperature, neutrons_emitted):
-    '''
+"""Meltdown Mitigation
 
-    :param temperature: int
-    :param neutrons_emitted: int
-    :return:  boolean True if conditions met, False if not
+https://exercism.org/tracks/python/exercises/meltdown-mitigation
+"""
 
-    A reactor is said to be critical if it satisfies the following conditions:
+
+def is_criticality_balanced(temperature: int,
+                            neutrons_emitted: int) -> bool:
+    """Test to see if the reactor is critically balanced
+
+    The reactor is critically balanced when:
+
     - The temperature is less than 800.
     - The number of neutrons emitted per second is greater than 500.
-    - The product of temperature and neutrons emitted per second is less than 500000.
-    '''
+    - The product of temperature and neutrons emitted per second is less than
+      500_000.
 
-    pass
+    Args:
+        temperature (int): The temperature of the reactor
+        neutrons_emitted (int): The number of neurons emitted per second
+
+    Returns:
+        bool: Is the reactor critically balanced
+    """
+
+    product_of_temperature_and_neutrons = temperature * neutrons_emitted
+
+    return (temperature < 800
+            and neutrons_emitted > 500
+            and product_of_temperature_and_neutrons < 500_000)
 
 
-def reactor_efficiency(voltage, current, theoretical_max_power):
-    '''
+def reactor_efficiency(voltage: int,
+                       current: int,
+                       theoretical_max_power: int) -> str:
+    """Calculate reactor efficiency band
 
-    :param voltage: int
-    :param current: int
-    :param theoretical_max_power: int
-    :return: str one of 'green', 'orange', 'red', or 'black'
-
-    Efficiency can be grouped into 4 bands:
+    Once the reactor has started producing power its efficiency needs to be
+    determined. Efficiency can be grouped into 4 bands:
 
     1. green  ->   80-100% efficiency
     2. orange ->   60-79% efficiency
     3. red    ->   30-59% efficiency
     4. black  ->   <30% efficient
 
-    These percentage ranges are calculated as
-    (generated power/ theoretical max power)*100
-    where generated power = voltage * current
-    '''
+    These percentage ranges are calculated as `(generated power / theoretical
+    max power) * 100` where `generated power = voltage * current`
 
-    pass
+    Args:
+        voltage (int): The reactor's voltage
+        current (int): The reactor's current
+        theoretical_max_power (int): The reactor's theoretical max power
+
+    Returns:
+        str: The efficiency band colour
+    """
+
+    generated_power = voltage * current
+    efficiency = (generated_power / theoretical_max_power) * 100
+
+    # Use interval comparison
+    # https://stackoverflow.com/questions/24436150/how-does-interval-comparison-work
+    if 80 <= efficiency <= 100:
+        return "green"
+    if 60 <= efficiency <= 79:
+        return "orange"
+    if 30 <= efficiency <= 59:
+        return "red"
+    return "black"
 
 
-def fail_safe(temperature, neutrons_produced_per_second, threshold):
-    '''
+def fail_safe(temperature: int,
+              neutrons_produced_per_second: int,
+              threshold: int) -> str:
+    """Failsafe mechanism
 
-    :param temperature:
-    :param neutrons_produced_per_second:
-    :param threshold:
-    :return: str one of: 'LOW', 'NORMAL', 'DANGER'
+    If temperature * neutrons_produced_per_second < 40% of threshold, output a
+    status code of 'LOW' indicating that control rods must be removed to
+    produce power.
 
-    - `temperature * neutrons per second` < 40% of threshold == 'LOW'
-    - `temperature * neutrons per second` +/- 10% of `threshold` == 'NORMAL'
-    - `temperature * neutron per second` is not in the above-stated ranges ==  'DANGER'
-    '''
+    If temperature * neutrons_produced_per_second are within plus or minus 10%
+    of the threshold the reactor is in criticality and the status code of
+    'NORMAL' should be output, indicating that the reactor is in optimum
+    condition and control rods are in an idea position.
 
-    pass
+    If temperature * neutrons_produced_per_second is not in the above-stated
+    ranges, the reactor is going into meltdown and a status code of 'DANGER'
+    must be passed to immediately shut down the reactor.
+
+
+    Args:
+        temperature (int): Temperature of reactor
+        neutrons_produced_per_second (int): Number of neutrons the reactor is
+            producing per second
+        threshold (int): The reactor criticality threshold
+
+    Returns:
+        str: Status code
+    """
+
+    temp_and_neutrons = temperature * neutrons_produced_per_second
+    percent_of_threshold_40 = 0.4 * threshold
+    percent_of_threshold_10 = 0.1 * threshold
+    normal_lbound = threshold - percent_of_threshold_10
+    normal_ubound = threshold + percent_of_threshold_10
+
+    if temp_and_neutrons < percent_of_threshold_40:
+        return "LOW"
+    if normal_lbound <= temp_and_neutrons <= normal_ubound:
+        return "NORMAL"
+    return "DANGER"
